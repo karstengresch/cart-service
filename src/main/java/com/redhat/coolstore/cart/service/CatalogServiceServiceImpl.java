65544dd30122b4f8c97f067edb07e1648be2b0b1
Karsten Gresch <git-2015@by-doing.org>
2017-10-05 16:14:01 +0200

    CatalogServiceServiceImpl

---
+++ src/main/java/com/redhat/coolstore/cart/service/CatalogServiceServiceImpl.java
@@ -0,0 +1,14 @@
+package com.redhat.coolstore.cart.service;
+
+import com.redhat.coolstore.cart.model.Product;
+
+/**
+ * Created by Karsten Gresch on 05.10.17.
+ */
+public class CatalogServiceServiceImpl implements CatalogService
+{
+  @Override public Product getProduct(String itemId)
+  {
+    return null;
+  }
+}
